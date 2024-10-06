d0512ef12346b3a3176e4c9cfa131fdebb750db9
umutbide1 <umut.bide.564@gmail.com>
2024-10-06 19:04:49 +0300

    video kayit ve veri olusturma kismi son durum

--- videoKayit.py
+++ videoKayit.py
@@ -1,25 +1,25 @@
 import cv2 
 
-cap=cv2.VideoCapture(0)
-width=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-print(width,height)
+print(width, height)
 
-# video kaydedici kısmı 
+# Video kaydedici kısmı
+writer = cv2.VideoWriter("video_kaydi_deneme.mp4", cv2.VideoWriter_fourcc(*"DIVX"), 30, (width, height))
 
-writer= cv2.VideoWriter("video_kaydi_deneme.mp4",cv2.VideoWriter_fourcc(*"DIVX"),30,(width,height)) # cerceveleri sikistirmak icin kullanilan kodec kodu imis fourcc kismi
-
-#video kaydetme kismi vesaire 
-
+# Video kaydetme kısmı
 while True:
-    ret,frame = cap.read()
-    cv2.imshow("video",frame)
-    
-    writer.write(frame)
-    
-    if cv2.waitKey(1) & 0xFF == ord("q") : break
+    ret, frame = cap.read()
+    cv2.imshow("video", frame)
 
-cap.relase()
-writer.relase()
+    writer.write(frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"): 
+        break
+
+# Kaynakları serbest bırakma kısmı
+cap.release()  # relase değil release olmalı
+writer.release()  # relase değil release olmalı
 cv2.destroyAllWindows()

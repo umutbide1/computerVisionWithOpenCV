bfde7254b54572f34341a34cfa1621b5cc62a691
umutbide1 <umut.bide.564@gmail.com>
2024-10-16 11:25:15 +0300

    Cizgi cizdirme ve line fonksiyon kullanimi

--- sekillerVeMetin/sekiller.py
+++ sekillerVeMetin/sekiller.py
@@ -7,7 +7,11 @@ import numpy as np
 img = np.zeros((512,512,3), np.uint8) # siyah resim olusturmaya yarayan dizin 
                                       # 512 512 3 bu resmin shape i 3 değil de 1 olsaydı renksiz olacaktı resim
 print("Şeklin boyutu: ",img.shape)    # resmin boyutunu doğru ayarlamış mıyız diye kontrol ediyoruz 
-cv2.imshow("Numpy kutuphanesi tarafindan olusturulan goruntu: ", img) 
-#commit bağlantı deneme 
+#cv2.imshow("Numpy kutuphanesi tarafindan olusturulan goruntu: ", img) 
 
+# resime çizgi ekleme konusunu ele alıyoruz
+# sırasıyla (resim,baslangic noktasi,bitis noktasi,renk,kalinlik)
+# renk muhabbetinde openCV de şu muhabbet var renk RGB değil de BGR şeklinde ilerliyor
+cv2.line(img,(0,0),(512,512),(255,0,0),(5))      
+cv2.imshow("Çizgi", img)           
 

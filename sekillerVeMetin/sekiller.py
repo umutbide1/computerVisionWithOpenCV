7219bcb3b68dfadf6648fcb6ee4375f8452f0353
umutbide1 <umut.bide.564@gmail.com>
2024-10-16 11:14:10 +0300

    github baglanti denemesi

--- sekillerVeMetin/sekiller.py
+++ sekillerVeMetin/sekiller.py
@@ -8,3 +8,6 @@ img = np.zeros((512,512,3), np.uint8) # siyah resim olusturmaya yarayan dizin
                                       # 512 512 3 bu resmin shape i 3 değil de 1 olsaydı renksiz olacaktı resim
 print("Şeklin boyutu: ",img.shape)    # resmin boyutunu doğru ayarlamış mıyız diye kontrol ediyoruz 
 cv2.imshow("Numpy kutuphanesi tarafindan olusturulan goruntu: ", img) 
+#commit bağlantı deneme 
+
+

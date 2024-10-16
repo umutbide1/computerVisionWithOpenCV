704c6cec4a361856c154d9e80cdee6997b02ea3e
umutbide1 <umut.bide.564@gmail.com>
2024-10-16 18:59:27 +0300

    Goruntu Perspektiflendirme

--- perspectifeCarpitma/perspektifCarpitma.py
+++ perspectifeCarpitma/perspektifCarpitma.py
@@ -4,8 +4,8 @@ import numpy as np
 img= cv2.imread("kart.png")
 cv2.imshow("Kart", img)
 
-width=400
-height=500
+width=500
+height=600
 # kartın 4 köşesini bulmaya çalışacağız 
 # paintten bakıyoruz sol ust kose : 201 e 1    sağ üst kose : 541 146   sag alt kose : 339 617  sol alt kose : 1 472 
 
@@ -14,8 +14,8 @@ point1= np.float32([[201,1],[1,472],  # bura benim yamuk resmimin kenarlarının
 
 point2 = np.float32([[0,0],[0,height],[width,0],[width,height]])
 
-transformMatris = cv2.getPerspectiveTransform(point1, point2)
-print(transformMatris)
+transformMatris = cv2.getPerspectiveTransform(point1, point2)   # transform matris hesabının yapılması
+print(transformMatris) # burada transform matrisin değerleri alınabilir
 
-imgLastStatus = cv2.warpPerspective(img, transformMatris, (width,height))
+imgLastStatus = cv2.warpPerspective(img, transformMatris, (width,height)) 
 cv2.imshow("Son Durum", imgLastStatus)

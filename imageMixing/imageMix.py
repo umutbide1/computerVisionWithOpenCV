821f975740b9b6e56b757d87f4728f28e66c0835
umutbide1 <umut.bide.564@gmail.com>
2024-10-17 19:41:09 +0300

    Resim içeri aktarma ve matplotlib kullanımı ile fotoğrafları koordinat düzlemine taşıma

--- imageMixing/imageMix.py
+++ imageMixing/imageMix.py
@@ -1,12 +1,17 @@
 import cv2 
-import numpy as np
-import pandas as pd 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 img1= cv2.imread("img1.JPG")
 img2= cv2.imread("img2.JPG")
 img3= cv2.imread("img3.jpg")
 
-cv2.imshow("img1", img1)
-cv2.imshow("img2", img2)
-cv2.imshow("img3", img3)
+# cv2.imshow("img1", img1)
+# cv2.imshow("img2", img2)
+# cv2.imshow("img3", img3)
+
+plt.figure()
+plt.imshow(img1)
+
+plt.figure()
+plt.imshow(img2)
+

c56b0e3372434b5a9ad41c8838277cddf75d88f6
umutbide1 <umut.bide.564@gmail.com>
2024-10-17 20:19:20 +0300

    BGR2RGB kısmının açıklamaları eklendi

--- imageMixing/imageMix.py
+++ imageMixing/imageMix.py
@@ -2,9 +2,13 @@ import cv2
 import matplotlib.pyplot as plt
 
 img1= cv2.imread("img1.JPG")
-img2= cv2.imread("img2.JPG")
-img3= cv2.imread("img3.jpg")
+img1= cv2.cvtColor(img1,cv2.COLOR_BGR2RGB)
 
+img2= cv2.imread("img2.JPG")
+img2= cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
+
+img3= cv2.imread("img3.jpg")
+img3= cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
 # cv2.imshow("img1", img1)
 # cv2.imshow("img2", img2)
 # cv2.imshow("img3", img3)
@@ -15,3 +19,5 @@ plt.imshow(img1)
 plt.figure()
 plt.imshow(img2)
 
+plt.figure()
+plt.imshow(img3)

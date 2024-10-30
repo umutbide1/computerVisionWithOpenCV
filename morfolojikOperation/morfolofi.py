69bbd15a1b76dea699287339963f9e25b4bbe2cc
umutbide1 <umut.bide.564@gmail.com>
2024-10-30 23:51:54 +0300

    Genişletilme konusu işlendi ve kodu yazıldı

--- morfolojikOperation/morfolofi.py
+++ morfolojikOperation/morfolofi.py
@@ -13,4 +13,9 @@ plt.figure(),plt.imshow(img),plt.axis("off"),plt.title("Orijinal Goruntu"), plt.
 kernel = np.ones((5,5),dtype=np.uint8)
 result = cv2.erode(img, kernel,iterations=1)
 
-plt.figure(),plt.imshow(result),plt.axis("off"),plt.title("Oynanmıs Goruntu"), plt.show()
+plt.figure(),plt.imshow(result),plt.axis("off"),plt.title("Erozyona ugramıs Goruntu"), plt.show()
+
+# şimdi de genişleteceğiz 
+
+result2 = cv2.dilate(img, kernel,iterations=3)
+plt.figure(),plt.imshow(result2),plt.axis("off"),plt.title("Genisletilmis Goruntu"), plt.show()

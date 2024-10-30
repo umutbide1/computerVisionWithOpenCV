ada88a12bbc62da4d4db84d705c54247abffed1e
umutbide1 <umut.bide.564@gmail.com>
2024-10-30 23:42:53 +0300

    Yazı kenarlarını inceltme işlemi incelendi

--- morfolojikOperation/morfolofi.py
+++ morfolojikOperation/morfolofi.py
@@ -1,3 +1,16 @@
 # beyaz bölgeleri açma ya da kapatma için kullanılır gürültü gidermede fayda sağlıyoruz
 
 # genişleme ve erozyon denen kavramları şimdi işleyeceğiz 
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv2.imread("datai.jpg")
+plt.figure(),plt.imshow(img),plt.axis("off"),plt.title("Orijinal Goruntu"), plt.show()
+
+# erozyon
+kernel = np.ones((5,5),dtype=np.uint8)
+result = cv2.erode(img, kernel,iterations=1)
+
+plt.figure(),plt.imshow(result),plt.axis("off"),plt.title("Oynanmıs Goruntu"), plt.show()

1921c8398c1bc91ec5687e22baa41db821ff176f
umutbide1 <umut.bide.564@gmail.com>
2024-11-03 22:59:35 +0300

    Gradianlarda x ve y ekseninde gradian fonksiyonun kullanımı işlenildi

---
+++ gradyanlar/gradian.py
@@ -0,0 +1,17 @@
+# görüntüdeki yoğunluk veya renkteki yönlü değişiklik imiş
+
+import cv2
+import matplotlib.pyplot as plt 
+
+img = cv2.imread("sudoku2.jpg",0)
+plt.figure(),plt.imshow(img,cmap="gray"),plt.axis("off"),plt.title("Sudoku resmi"),plt.show()
+
+# x gradyanını bulalım
+
+sobelX = cv2.Sobel(img, ddepth=cv2.CV_16S, dx=1, dy=0)
+plt.figure(),plt.imshow(sobelX,cmap="gray"),plt.axis("off"),plt.title("X ekseninde gradyani alinmis sudoku resmi"),plt.show()
+
+# y gradyanını bulalım 
+
+sobelY = cv2.Sobel(img, ddepth=cv2.CV_16S, dx=0, dy=1)
+plt.figure(),plt.imshow(sobelY,cmap="gray"),plt.axis("off"),plt.title("Y ekseninde gradyani alinmis sudoku resmi"),plt.show()

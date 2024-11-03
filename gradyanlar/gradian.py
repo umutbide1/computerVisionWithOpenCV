b1c7f06aec2a9bd313c02a0b4ad32f3398870d1c
umutbide1 <umut.bide.564@gmail.com>
2024-11-03 23:17:00 +0300

    Laplacian Gradyan Fonksiyonu incelendi

--- gradyanlar/gradian.py
+++ gradyanlar/gradian.py
@@ -3,7 +3,7 @@
 import cv2
 import matplotlib.pyplot as plt 
 
-img = cv2.imread("sudoku2.jpg",0)
+img = cv2.imread("sudoku.jpg",0)
 plt.figure(),plt.imshow(img,cmap="gray"),plt.axis("off"),plt.title("Sudoku resmi"),plt.show()
 
 # x gradyanını bulalım
@@ -15,3 +15,8 @@ plt.figure(),plt.imshow(sobelX,cmap="gray"),plt.axis("off"),plt.title("X eksenin
 
 sobelY = cv2.Sobel(img, ddepth=cv2.CV_16S, dx=0, dy=1)
 plt.figure(),plt.imshow(sobelY,cmap="gray"),plt.axis("off"),plt.title("Y ekseninde gradyani alinmis sudoku resmi"),plt.show()
+
+# iki tarafında aynı anda gradyanını almak istersem laplacian kullanıyorum
+
+laplacian = cv2.Laplacian(img, ddepth=cv2.CV_16S)
+plt.figure(),plt.imshow(laplacian,cmap="gray"),plt.axis("off"),plt.title("Laplacian"),plt.show()
